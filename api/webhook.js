12521efd12ef11f72df86f482fcb324d005f015a
Vicente De la Rosa <vicente.dela.rosa@hotmail.com>
2025-07-20 16:13:23 -0600

    Fix webhook verification

--- api/webhook.js
+++ api/webhook.js
@@ -1,24 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 app.use(express.json());
 
 const VERIFY_TOKEN = "taxici_token_123";
 
-app.get("/webhook", (req, res) => {
-  const mode = req.query["hub.mode"];
-  const token = req.query["hub.verify_token"];
-  const challenge = req.query["hub.challenge"];
+// Ruta para la verificación del webhook (GET)
+app.get('/webhook', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
 
-  if (mode === "subscribe" && token === VERIFY_TOKEN) {
-    console.log("WEBHOOK_VERIFIED");
+  if (mode && token === VERIFY_TOKEN) {
+    console.log("WEBHOOK VERIFICADO");
     res.status(200).send(challenge);
   } else {
     res.sendStatus(403);
   }
 });
 
-app.post("/webhook", (req, res) => {
-  console.log("Mensaje recibido:", JSON.stringify(req.body, null, 2));
+// Ruta para recibir mensajes (POST)
+app.post('/webhook', (req, res) => {
+  console.log('Mensaje recibido:', JSON.stringify(req.body, null, 2));
   res.sendStatus(200);
 });
 

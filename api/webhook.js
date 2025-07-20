ebea1a031c4a42d470690ef886f131705133678a
Vicente De la Rosa <vicente.dela.rosa@hotmail.com>
2025-07-20 16:00:28 -0600

    Corrijo estructura y vercel.json

--- api/webhook.js
+++ api/webhook.js
@@ -1,9 +1,6 @@
-const awsServerlessExpress = require("aws-serverless-express");
 const express = require("express");
-const bodyParser = require("body-parser");
-
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 const VERIFY_TOKEN = "taxici_token_123";
 
@@ -12,22 +9,17 @@ app.get("/webhook", (req, res) => {
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  if (mode && token && mode === "subscribe" && token === VERIFY_TOKEN) {
-    console.log("✅ Verificado con éxito.");
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    console.log("WEBHOOK_VERIFIED");
     res.status(200).send(challenge);
   } else {
-    console.log("❌ Falló la verificación.");
     res.sendStatus(403);
   }
 });
 
 app.post("/webhook", (req, res) => {
-  console.log("📩 POST recibido:", JSON.stringify(req.body, null, 2));
+  console.log("Mensaje recibido:", JSON.stringify(req.body, null, 2));
   res.sendStatus(200);
 });
 
-const server = awsServerlessExpress.createServer(app);
-
-module.exports = (req, res) => {
-  awsServerlessExpress.proxy(server, req, res);
-};
+module.exports = app;

5b71709c662bc452348aa5d0fae0889bb1c5f265
Vicente De la Rosa <vicente.dela.rosa@hotmail.com>
2025-07-24 15:32:26 -0600

    Actualizar webhook.js para verificación

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,5 @@
 
-
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   const VERIFY_TOKEN = "taxici_token_2025_2";
 
   if (req.method === "GET") {
@@ -8,16 +7,23 @@ module.exports = async (req, res) => {
     const token = req.query["hub.verify_token"];
     const challenge = req.query["hub.challenge"];
 
-    if (mode && token && mode === "subscribe" && token === VERIFY_TOKEN) {
-      console.log("WEBHOOK_VERIFIED");
-      res.status(200).send(challenge);
+    if (mode && token) {
+      if (mode === "subscribe" && token === VERIFY_TOKEN) {
+        console.log("WEBHOOK_VERIFIED");
+        res.status(200).send(challenge);
+        return;
+      } else {
+        return res.status(403).send("Token inválido");
+      }
     } else {
-      res.status(403).send("Token inválido");
+      return res.status(400).send("Faltan parámetros");
     }
-  } else if (req.method === "POST") {
-    console.log("Mensaje recibido:", JSON.stringify(req.body, null, 2));
-    res.sendStatus(200);
-  } else {
-    res.status(405).send("Método no permitido");
   }
-};
+
+  if (req.method === "POST") {
+    console.log("Mensaje recibido:", JSON.stringify(req.body, null, 2));
+    return res.status(200).send("Mensaje procesado");
+  }
+
+  return res.status(405).send("Método no permitido");
+}

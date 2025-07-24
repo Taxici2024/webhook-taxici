b8859cccee52417e54463cb844e657c5b7e638b1
Vicente De la Rosa <vicente.dela.rosa@hotmail.com>
2025-07-24 13:30:43 -0600

    Webhook corregido en carpeta api

---
+++ api/webhook.js
@@ -0,0 +1,30 @@
+export default async function handler(req, res) {
+  const VERIFY_TOKEN = "taxici_token_2025_2";
+
+  if (req.method === "GET") {
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+
+    if (mode && token) {
+      if (mode === "subscribe" && token === VERIFY_TOKEN) {
+        console.log("WEBHOOK_VERIFIED");
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(challenge);
+        return;
+      } else {
+        return res.status(403).end("Token inválido");
+      }
+    } else {
+      return res.status(400).end("Faltan parámetros");
+    }
+  }
+
+  if (req.method === "POST") {
+    console.log("Mensaje recibido:", JSON.stringify(req.body, null, 2));
+    return res.status(200).end("Mensaje procesado");
+  }
+
+  return res.status(405).end("Método no permitido");
+}
